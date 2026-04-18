6d49f2da84b7dba45ea40270278cf21141357a2d
Manvendra Yadav <manvendra.yadav@flipkart.com>
2026-04-18 14:58:27 +0530

    minor ui

--- frontend/src/components/sidebar/ChatWidget/ChatWidget.jsx
+++ frontend/src/components/sidebar/ChatWidget/ChatWidget.jsx
@@ -3,8 +3,7 @@ import styles from './ChatWidget.module.css'
 export default function ChatWidget() {
   return (
     <div className={styles.widget}>
-      <img src="/static/cat.gif" alt="Cat mascot" className={styles.avatar} />
-      <div className={styles.bubble}>
+<div className={styles.bubble}>
         Hey there! 👋 Enjoying OneSearch? Let us know what you&apos;d like to see next!
       </div>
     </div>
